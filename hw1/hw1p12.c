0992f9bb7309e6744604b2c837144e12c1c8dd99
carmi <evan@ecarmi.org>
2010-09-21 21:52:41 -0400

    Think I have rec_merge working

--- hw1/hw1p12.c
+++ hw1/hw1p12.c
@@ -24,7 +24,8 @@
 
 void merge_sort_rec(int *xs, size_t size, int(*compare)(int, int));
 void merge_sort_itr(int *xs, size_t size, int(*compare)(int, int));
-
+void merge(int *output, int *first, int first_size, int *second, int second_size,
+        int(*compare)(int, int));
 /** Sort an array of integers according to a supplied comparison function. The
  * elements of the array will be rearranged into non-decreasing order according
  * to the comparison function.
@@ -35,94 +36,85 @@ void merge_sort_itr(int *xs, size_t size, int(*compare)(int, int));
  * a number < 0, 0, or a number > 0 as per x < y, x = y, or x > y.
  */
 void merge_sort_rec(int *xs, size_t size, int(*compare)(int, int)) {
-    // Base case: return if size is less than one.
-    if (size <= 1)
-        return *xs;
-    
-    // Recursive case: separate into two parts and recursively sort list.
+    // Base case: if size is one or zero assume list is sorted and return.
+    if (size <= 1) return;
+
+    // Recursive case: divide into two parts and recursively sort list.
     int middle = size/2;
-    
+
+    // Create new arrays to hold each part of xs and populate them from xs.
     int first[middle]; // First half of array xs
     int second[(size - middle)]; // Second half of array xs
-    
     for (int i = 0; i < middle; ++i)
         first[i] = xs[i];
     for (int i = middle; i < size; ++i)
         second[i] = xs[i];
-    
-    first = merge_sort_rec(first, middle, compare);
-    second = merge_sort_rec(second, middle, compare);
-    
-    return merge(first, middle, second, (size - middle), compare);
 
+    // Recursively sort both parts of divided array.
+    merge_sort_rec(first, middle, compare);
+    merge_sort_rec(second, middle, compare);
+    
+    // After the above recursive sorts return, we can assume that first and
+    // second are each sorted in non-decreasing order. We then merge them into
+    // xs.
+    merge(xs, first, middle, second, (size - middle), compare);
+}
 /**
  * Merge - merge two integer arrays together into a single array in
- * non-descending order. It is a precondition that each input array is already
- * sorted in non-descending order.
+ * non-decreasing order. It is a precondition that each input array is already
+ * sorted in non-decreasing order.
  *
- * @param first - the first array; must be sorted in non-descending order.
+ * Effect: the output array will contain all elements from first and second and
+ *          be in non-decreasing order.
+ *
+ * @param output - the output array; must be an array of length (first_size +
+ *          second_size)
+ * @param first - the first array; must be sorted in non-decreasing order.
  * @param first_size - the length of first.
- * @param second - the second array; must be sorted in non-descending order.
+ * @param second - the second array; must be sorted in non-decreasing order.
  * @param second_size - the length of second.
  * @param compare - the comparison function. compare(x, y) returns
- * a number < 0, 0, or a number > 0 as per x < y, x = y, or x > y.
+ *          a number < 0, 0, or a number > 0 as per x < y, x = y, or x > y.
  */
-void merge(int *first, int first_size, int *second, int second_size,
+void merge(int *output, int *first, int first_size, int *second, int second_size,
         int(*compare)(int, int)) {
-    // Create a result array and populate it.
-    int result[(first_size + second_size)];
-    int result_index = 0;
-    
+    // Create indices to keep track of position in arrays
+    int output_index = 0;
     int first_index = 0;
     int second_index = 0;
     while ((first_index < first_size) || (second_index < second_size))
     {
         if ((first_index < first_size) && (second_index < second_size))
         {
-            // Compare first items of each array, and place smaller in result.
+            // Compare first items of each array, and place smaller in output.
             if (first[first_index] <= second[second_index])
             {
-                result[result_index] = first[first_index];
-                result_index++;
+                output[output_index] = first[first_index];
+                output_index++;
                 first_index++;
             }
             else
             {
-                result[result_index] = second[second_index];
-                result_index++;
+                output[output_index] = second[second_index];
+                output_index++;
                 second_index++;
             }
         }
         else if (first_index < first_size)
         {
-            result[result_index] = first[first_index];
-            result_index++;
+            output[output_index] = first[first_index];
+            output_index++;
             first_index++;
         }
         else if (second_index < second_size)
         {
-            result[result_index] = second[second_index];
-            result_index++;
+            output[output_index] = second[second_index];
+            output_index++;
             second_index++;
         }
     }
-    return result;
+    return;
 }
-
-
-
-
-
-
-    
-
-}
-
-
-
-
-
 
 void merge_sort_itr(int *xs, size_t size, int(*compare)(int, int)) {
 }
-
